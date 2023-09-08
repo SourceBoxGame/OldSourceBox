90ceb265986ce85aed685eaec1b3a3110d95c56b
relt-1 <60782515+relt-1@users.noreply.github.com>
2023-09-08 09:29:26 +0200

    go away

--- pythoninterface/pythoninterface.cpp
+++ pythoninterface/pythoninterface.cpp
@@ -156,7 +156,7 @@ static int Python_Set_QObject_Member(PyObject* obj, PyObject* val, void* ch)
     case QType_String:
         child->value_string = PyUnicode_AsUTF8(val); break;
     default:
-        PyErr_Format(PyExc_TypeError, "%s.%s has invalid type (%i) while trying to set it to %s", obj, child->name, child->type, PyUnicode_AsUTF8(PyObject_Str(val)));
+        PyErr_Format(PyExc_TypeError, "%s.%s has invalid type (%i) while trying to set it to %s", obj->ob_type->tp_name, child->name, child->type, PyUnicode_AsUTF8(PyObject_Str(val)));
         return -1;
     }
     return 0;
@@ -336,51 +336,6 @@ void CPythonInterface::CallCallback(QCallback* callback, QArgs* args)
 
 
 
-
-
-
-typedef struct {
-    PyObject_HEAD
-        /* Type-specific fields go here. */
-} CustomObject;
-
-static PyTypeObject CustomType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "custom.Custom",
-    .tp_doc = PyDoc_STR("Custom objects"),
-    .tp_basicsize = sizeof(CustomObject),
-    .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_new = PyType_GenericNew,
-};
-
-static PyModuleDef custommodule = {
-    PyModuleDef_HEAD_INIT,
-    .m_name = "custom",
-    .m_doc = "Example module that creates an extension type.",
-    .m_size = -1,
-};
-
-PyMODINIT_FUNC
-PyInit_custom(void)
-{
-    PyObject* m;
-    if (PyType_Ready(&CustomType) < 0)
-        return NULL;
-
-    m = PyModule_Create(&custommodule);
-    if (m == NULL)
-        return NULL;
-
-    Py_INCREF(&CustomType);
-    if (PyModule_AddObject(m, "Custom", (PyObject*)&CustomType) < 0) {
-        Py_DECREF(&CustomType);
-        Py_DECREF(m);
-        return NULL;
-    }
-
-    return m;
-}
 
 
 
